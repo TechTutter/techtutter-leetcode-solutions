2c2b5db2e2a624251f8ac9231078c04eacf812ed
TechTutter <f.lorrai.95@gmail.com>
2026-02-19 21:15:10 +0100

    EX-69

---
+++ leetproblems/69. SqrtX/solution_test.py
@@ -0,0 +1,13 @@
+from solution import Solution
+
+
+class TestExerciseName:
+    def test_testcase1(self):
+        assert Solution().mySqrt(4) == 2
+
+    def test_testcase2(self):
+        assert Solution().mySqrt(8) == 2
+
+    def test_testcase3(self):
+        assert Solution().mySqrt(0) == 0
+
